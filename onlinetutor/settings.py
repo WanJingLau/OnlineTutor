deb62298f77781a2e92a8a10a388fae8b81067d2
wanjing <p20012339@student.newinti.edu.my>
2022-01-18 18:18:49 +0800

    add db conn

--- onlinetutor/settings.py
+++ onlinetutor/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'onlinetutor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'onlinetutor',
+        'USER': 'user123',
+        'PASSWORD': 'user123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
